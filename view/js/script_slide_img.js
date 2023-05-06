db60982e825b21b04105563c1765dd27bb4929d4
Ildefonso Lares <65493114+Flayxamax@users.noreply.github.com>
2023-05-05 23:45:34 -0700

    Se agregó pagina index

---
+++ view/js/script_slide_img.js
@@ -0,0 +1,23 @@
+var bannerImg = document.querySelector('.banner .banner-image img');
+var currentPosition = 0;
+
+function slideImage() {
+  currentPosition -= 5;
+  bannerImg.style.right = currentPosition + 'px';
+  if (currentPosition <= -(bannerImg.width)) {
+    currentPosition = bannerImg.width;
+  }
+}
+
+setInterval(slideImage, 10);
+
+window.onload = function() {
+  var slide = document.getElementById("slide");
+  slide.style.right = "100%";
+  slide.style.opacity = "0";
+  setTimeout(function() {
+    slide.style.transition = "all 0.8s ease-out";
+    slide.style.right = "0";
+    slide.style.opacity = "1";
+  }, 500);
+};
